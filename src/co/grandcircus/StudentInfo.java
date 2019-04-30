c1309284c05d873de94c6244df5289d74b626791
adamjsmith15 <ajsmith015@gmail.com>
2019-04-30 16:52:01 -0400

    Added comments and formatting

--- src/co/grandcircus/StudentInfo.java
+++ src/co/grandcircus/StudentInfo.java
@@ -6,20 +6,22 @@ import java.util.Scanner;
 public class StudentInfo {
 
 	public static void main(String[] args) {
+		// initialize scanner
 		Scanner scnr = new Scanner(System.in);
-		String studentNames[] = { "Adam", "Laura", "Lillian", "Rohit", "Linmei", "Carl" };
-		String studentHomeTown[] = { "Charlotte", "Westland", "Eaton Rapids", "Novi", "China", "Lansing" };
+		// initialize arrays
+		String studentNames[] = { "Adam", "Laura", "Lillian", "Rohit", "Zane", "Carl" };
+		String studentHomeTown[] = { "Charlotte", "Westland", "Eaton Rapids", "Novi", "St Johns", "Lansing" };
 		String studentFood[] = { "Pizza", "Pizza", "Pasta", "Pad Thai", "Burgers", "Tacos" };
 
+		// initialize userNum to be used outside of methods
 		int userNum;
 		String keepPlaying = "yes";
+		
 
+		// checks to see if player would like to keep playing
 		while (keepPlaying.equalsIgnoreCase("yes")) {
 
-			// getName(studentNames, scnr, "Which student would you like to learn more
-			// about? (Enter a number 1-20): ");
-			userNum = getName(studentNames, scnr,
-					"Which student would you like to learn more about? (Enter a number 1-6): ");
+			userNum = getName(studentNames, scnr, "Which student would you like to learn more about? (Enter a number 1-6): ");
 
 			System.out.print(infoDecider(userNum, scnr, "", studentNames, studentHomeTown, studentFood));
 
@@ -32,7 +34,9 @@ public class StudentInfo {
 
 	}
 
+	//int methods takes in an array, scanner and prompt
 	public static int getName(String arr[], Scanner scnr, String prompt) {
+		//initialize boolean to false so you can go into while loop
 		boolean boole = false;
 		String name = "";
 		int userNum = 0;
@@ -40,6 +44,7 @@ public class StudentInfo {
 
 		while (boole == false) {
 			
+			//try catch to check for a input mismatch and continue to ask for input until input is correct
 			try {
 				userNum = scnr.nextInt();
 			} catch(InputMismatchException e) {
@@ -49,6 +54,8 @@ public class StudentInfo {
 				scnr.nextLine();
 				continue;
 			} 
+			
+			// try catch to check for number within bounds of index and get input until num is in bounds
 			try {
 
 				name = (arr[userNum - 1]);
@@ -66,27 +73,37 @@ public class StudentInfo {
 			}
 		}
 		System.out.println(" ");
+		//sysout results
 		System.out.print("Student " + userNum + " is " + name + ".  What would you like to know about " + name
 				+ "? (enter \"hometown\" or \"favorite food\"); ");
+		
+		//return userNum to be used outside of method
 		return userNum - 1;
 	}
 
+	// method that returns a string taking in user num and all of the arrays to decide appropriate response
 	static public String infoDecider(int userNum, Scanner scnr, String prompt, String arr1[], String arr2[],
 			String arr3[]) {
 		System.out.println(prompt);
 		String result = "";
 		// garbage line
 		scnr.nextLine();
+		// initialize boolean to enter while loop
 		boolean boole = false;
+		// continue checking until meets conditions of hometown or favorite food
 		while (boole != true) {
 
 			String answer = scnr.nextLine();
+			//  check input until it matches hometown or favorite 
 			if (answer.equalsIgnoreCase("hometown")) {
 				result = arr1[userNum] + " is from " + arr2[userNum] + ".";
+				
+				//to escape the while loop
 				boole = true;
 
 			} else if (answer.equalsIgnoreCase("favorite food")) {
 				result = arr1[userNum] + "'s favorite food is " + arr3[userNum] + ".";
+				//to escape the while loop
 				boole = true;
 
 			} else {
@@ -98,7 +115,8 @@ public class StudentInfo {
 		}
 		return result;
 	}
-
+	
+	// method to take in input and return entry for use of keep playing loop
 	static public String keepPlaying(Scanner scnr, String prompt) {
 		System.out.print(prompt);
 		String userEntry = scnr.next();
